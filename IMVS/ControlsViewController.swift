c304c2163a6ca7a6ea94c7da0a04c75122ebd661
Allistair Crossley <allistair@gmail.com>
2014-08-15 22:17:04 +0100

    Might be used later for controls view.

---
+++ IMVS/ControlsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ControlsViewController.swift
+//  IMVS
+//
+//  Created by Allistair Crossley on 27/07/2014.
+//  Copyright (c) 2014 Allistair Crossley. All rights reserved.
+//
+
+import UIKit
+
+class ControlsViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeRenderStyle(sender: AnyObject) {
+    }
+    
+    @IBAction func changeColourScheme(sender: AnyObject) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
