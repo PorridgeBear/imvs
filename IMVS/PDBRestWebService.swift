89680d23845e94a838d1dffe737c0080046a6be3
Allistair Crossley <allistair@gmail.com>
2014-08-15 22:15:19 +0100

    Beginnings of web service.

---
+++ IMVS/PDBRestWebService.swift
@@ -0,0 +1,36 @@
+//
+//  PDBRestWebService.swift
+//  IMVS
+//
+//  Created by Allistair Crossley on 27/07/2014.
+//  Copyright (c) 2014 Allistair Crossley. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * http://www.rcsb.org/pdb/rest/describePDB?structureId=1crn
+ * http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat=pdb&compression=NO&structureId=1crn
+ * http://www.rcsb.org/pdb/rest/search
+ */
+
+class PDBRestWebService {
+    
+    func searchByTitle(title: String) {
+    
+    /*
+    <orgPdbQuery>
+    
+    <queryType>org.pdb.query.simple.StructTitleQuery</queryType>
+    
+    <description>StructTitleQuery: struct.title.comparator=contains struct.title.value=Solution NMR structure of protein </description>
+    
+    <struct.title.comparator>contains</struct.title.comparator>
+    
+    <struct.title.value>Solution NMR structure of protein</struct.title.value>
+    
+    </orgPdbQuery>
+    */
+    
+    }
+}
