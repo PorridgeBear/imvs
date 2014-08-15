cf04a9e08bc2fb5126ac86dde09ad41ab5ac03cb
Allistair Crossley <allistair@gmail.com>
2014-08-15 22:16:55 +0100

    Removed Core Data stuff.

--- IMVS/AppDelegate.swift
+++ IMVS/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
+    
+    
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
@@ -40,7 +42,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
